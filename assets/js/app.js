f360759054bdabb3e22395a29ad70227b32c0369
Romans Špiļaks <romans.spilaks@gmail.com>
2023-04-10 05:14:44 +0300

    Move navigation out of wrapper, add toggle button for burger, create burger as svg, add styles to burger in different states (enabled, disabled). Create app.js, link js script to HTML document. Add function toggleBurger. Add on click events for 3 cases (click outside the area of  navigation, click on nav list item, click on burger button).

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,31 @@
 const topNav = document.querySelector('.top-navigation');
 const navToggle = document.querySelector('.nav-toggle');
+const navLink = document.querySelectorAll('.nav-link');
+const wrapper = document.querySelector('.wrapper');
 
-navToggle.addEventListener("click", () => {
+function toggleBurger () {
   const visibility = topNav.getAttribute('data-visible');
   if (visibility === "false") {
+    navToggle.setAttribute("aria-expanded", true)
     topNav.setAttribute("data-visible", true);
     topNav.setAttribute("aria-expanded", true)
   }
     else if (visibility === "true") {
+      navToggle.setAttribute("aria-expanded", false)
       topNav.setAttribute('data-visible', false);
       topNav.setAttribute("aria-expanded", false)
     }
+    console.log(visibility)
+};
+
+wrapper.addEventListener("click", () => {
+  toggleBurger();
+})
+
+navToggle.addEventListener("click", () => {
+ toggleBurger();
 });
+
+navLink.forEach(navLink => navLink.addEventListener("click", () => {
+toggleBurger();
+}))
