5b0398f3289d9329b753bec4b10b98dc916023e0
Romans Špiļaks <romans.spilaks@gmail.com>
2023-05-07 13:30:51 +0300

    Optimize webpack config file. Change link to manifest and add additional icons

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,12 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-
 const isProduction = process.env.NODE_ENV === 'production';
+const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
 
-const stylesHandler = isProduction
-  ? MiniCssExtractPlugin.loader
-  : 'style-loader';
-
-const config = {
+module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -24,13 +18,11 @@ const config = {
     }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: './src/favicon/apple-touch-icon.png', to: './favicon/apple-touch-icon.png' },
-        { from: './src/favicon/favicon-32x32.png', to: './favicon/favicon-32x32.png' },
-        { from: './src/favicon/favicon-16x16.png', to: './favicon/favicon-16x16.png' },
-        { from: './src/favicon/site.webmanifest', to: './favicon/site.webmanifest' },
+        { from: './src/favicon', to: './favicon' },
       ],
     }),
-  ],
+    isProduction && new MiniCssExtractPlugin(),
+  ].filter(Boolean),
 
   module: {
     rules: [
@@ -46,9 +38,6 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
 
@@ -59,15 +48,6 @@ const config = {
     compress: true,
     port: 8080,
   },
-};
 
-module.exports = () => {
-  if (isProduction) {
-    config.mode = 'production';
-
-    config.plugins.push(new MiniCssExtractPlugin());
-  } else {
-    config.mode = 'development';
-  }
-  return config;
+  mode: isProduction ? 'production' : 'development',
 };
