b2e65274f9d73e0256e348e335d5c2ed250b0b95
gersonhiggins <gahg.10gerson@gmail.com>
2023-05-11 13:27:35 -0400

    Test Change Description

--- src/modules/tests/clearChecked.test.js
+++ src/modules/tests/clearChecked.test.js
@@ -1,5 +1,9 @@
 import clearCompletedTasks from "../clearChecked.js";
+import changeDescription from "../changeDescription.js";
+import addTask from "../add.js";
 import { getTasks, setTasks } from "../storage.js";
+import expect from "expect";
+
 
 jest.mock('../storage', () => {
   let tasks = [];
@@ -17,7 +21,33 @@ describe("clearCompletedTasks", () => {
     expect(getTasks()).toHaveLength(2);
     clearCompletedTasks();
     expect(getTasks()).toHaveLength(1);
-    expect(getTasks()).toBe([
-    { description: "Task 2", completed: false, index: 1 }]);
+    expect(getTasks()[0]).toEqual({ description: "Task 2", completed: false, index: 1 });
   });
+});
+
+describe("changeDescription", () => {
+  beforeEach(() => {
+    setTasks([]);
+  });
+  it('should change task desription', () => {
+    document.body.innerHTML = `
+    <ul id="task-list">
+    </ul>
+  `;
+  const container = document.getElementById('task-list');
+  const addListInput = document.createElement('li');
+  const inputField = document.createElement('textarea');
+  const inputField2 = document.createElement('textarea');
+  addListInput.appendChild(inputField);
+  container.appendChild(addListInput);
+  const task1Description = 'Test task 1';
+  addTask(task1Description);
+  inputField.value = task1Description;
+  const task2 = "Test task 2"
+  inputField2.value = task2;
+  expect(getTasks()[0].description).toBe("Test task 1")
+  changeDescription(getTasks()[0], inputField2);
+  expect(getTasks()[0].description).toBe("Test task 2");
+  
+  })
 });
