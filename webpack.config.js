10eae2c417be7a18dd579f5f9e632edb092727eb
Romans Špiļaks <romans.spilaks@gmail.com>
2023-05-13 07:39:07 +0300

    Deploy page

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
       directory: path.join(__dirname, 'dist'),
     },
     compress: true,
-    port: 8080,
+    port: 8086,
   },
 
   mode: isProduction ? 'production' : 'development',
